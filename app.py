32a1e12c298385f198d1929f91faa8586aaca87a
Updesh Chauhan <49610779+updesh1@users.noreply.github.com>
2026-04-16 22:09:18 +0530

    Bug resolve

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ st.title("Bird vs Drone Classifier")
 
 if not os.path.exists("final_model.keras"):
     st.write("Downloading model...")
-    url = "https://drive.google.com/uc?id=1lRta1xmVufV2xFvm6lVNWH6UKpmbTceR"
+    url = "https://drive.google.com/uc?id=1cowtjeslqdeWzk-HnUkao3RkYTydy4c5"
     gdown.download(url, "final_model.keras", quiet=False)
 
 model = load_model("final_model.keras", compile=False)

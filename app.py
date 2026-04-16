aa8b61612d62995756f41a619817a0eb0a747ace
Updesh Chauhan <49610779+updesh1@users.noreply.github.com>
2026-04-16 17:00:27 +0530

    drive link update

--- app.py
+++ app.py
@@ -5,13 +5,11 @@ from tensorflow.keras.models import load_model
 
 st.title("Bird vs Drone Classifier")
 
-# Download model (.keras)
 if not os.path.exists("final_model.keras"):
     st.write("Downloading model...")
-    url = "https://drive.google.com/uc?id=19PlCSMEXGb6yHiM6AtIvP76xS_WyCSc1"
+    url = "https://drive.google.com/uc?id=1lRta1xmVufV2xFvm6lVNWH6UKpmbTceR"
     gdown.download(url, "final_model.keras", quiet=False)
 
-# Load model
 model = load_model("final_model.keras", compile=False)
 
 st.write("Model loaded successfully")

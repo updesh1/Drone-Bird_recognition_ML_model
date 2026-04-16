c123667988b0501ac03bbec1d574d70c2f2d7b91
Updesh Chauhan <49610779+updesh1@users.noreply.github.com>
2026-04-16 15:49:53 +0530

    Fixed missing os import

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+
+
+import os
+import gdown
+import streamlit as st
+from tensorflow.keras.models import load_model
+
+st.title("Bird vs Drone Classifier")
+
+# Download model
 if not os.path.exists("final_model.h5"):
+    st.write("Downloading model...")
     url = "https://drive.google.com/uc?id=1kjLDHmw4kU8385zQ8ZOllAIX1XDm6GTP"
     gdown.download(url, "final_model.h5", quiet=False)
 
+# Load model
 model = load_model("final_model.h5", compile=False)
 
-
-
-
+st.write("Model loaded successfully")
